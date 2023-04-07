fe216f5dce656d19848f8230555c8e9895dab5b3
TheOE07 <89691529+TheOE07@users.noreply.github.com>
2023-04-07 02:13:37 +0100

    Add files via upload

---
+++ FILE.js
@@ -0,0 +1,9 @@
+window.onload = gavin;
+
+function gavin(){
+    var stanhope = document.getElementById("stanhope");
+    stanhope.addEventListener("click", function() {
+      var audiotwo = new Audio("zogcrent.mp3");
+      audiotwo.play();
+    });
+}
